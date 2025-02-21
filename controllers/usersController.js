1c7b17b38849b35620d0c3b9a2ea0c2282e60572
tangwai <tmichael11314@gmail.com>
2025-02-21 05:17:54 -0600

    fix bcrypt >> password_hash

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -53,26 +53,28 @@ async function registerUser(req, res, next) {
 };
 
 passport.use(
-    new LocalStrategy(async (username, password, done) => {
-      try {
-        const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
-        const user = rows[0];
-  
-        if (!user) {
-          return done(null, false, { message: "Incorrect username" });
-        }
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+      const user = rows[0];
 
-        const match = await bcrypt.compare(password, user.password);
-
-        if (!match) {
-            return done(null, false, { message: "Incorrect password" });
-        }
-        return done(null, user);
-      } catch(err) {
-        return done(err);
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
       }
-    })
-  );
+
+      const match = await bcrypt.compare(password, user.password_hash);
+
+      if (!match) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
